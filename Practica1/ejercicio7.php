cf3592cd1c03fa3932922a7b21d9243ead5c6292
Rafael Aybar <rafaelaybar@hotmail.com>
2016-12-11 12:28:01 +0100

    Arreglado el title

--- Practica1/ejercicio7.php
+++ Practica1/ejercicio7.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8">
-        <title>Ejercicio6</title>
+        <title>Ejercicio 7</title>
         <style>
             h1 { font-size: 29px; font-family: Verdana; color:  #000000; }
             h4 { font-size: 20px; font-family: Verdana; color:  #000000; background: #efefef;}
