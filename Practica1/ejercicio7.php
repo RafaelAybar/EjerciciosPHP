9d870e818e43147c251f0fcc2b6c7ef22a3bb852
Rafael Aybar <rafaelaybar@hotmail.com>
2016-12-11 12:41:01 +0100

    Modificado el tamaño de letra

--- Practica1/ejercicio7.php
+++ Practica1/ejercicio7.php
@@ -3,36 +3,37 @@
         <meta charset="utf-8">
         <title>Ejercicio 7</title>
         <style>
-            h1 { font-size: 29px; font-family: Verdana; color:  #000000; }
             h4 { font-size: 20px; font-family: Verdana; color:  #000000; background: #efefef;}
             td { font-size: 20px; font-family: Verdana; color:  #000000; background: #efefef; }
             table{ border-collapse: collapse; box-shadow: 16px 16px 16px #cccccc; }
 </style>
  </head>
  <body>
-    <?php
-$tabla = "<table border='1'>";
-$tabla .= "<tbody>";
-for($i = 0; $i<10;$i++){
-    $tabla .= "<tr>";
-    for($j = 0; $j<10;$j++){
-        if($i == $j){
-            $tabla .= "<td>";
-            $tabla .= "1";
-            $tabla .= "</td>";
-        }else{
-            $tabla .= "<td>";
-            $tabla .= "0";
-            $tabla .= "</td>";
+     <h4>
+        <?php
+    $tabla = "<table border='1'>";
+    $tabla .= "<tbody>";
+    for($i = 0; $i<10;$i++){
+        $tabla .= "<tr>";
+        for($j = 0; $j<10;$j++){
+            if($i == $j){
+                $tabla .= "<td>";
+                $tabla .= "1";
+                $tabla .= "</td>";
+            }else{
+                $tabla .= "<td>";
+                $tabla .= "0";
+                $tabla .= "</td>";
+            }
+
         }
-
+        $tabla .= "</tr>";
     }
-    $tabla .= "</tr>";
-}
-$tabla .= "</tbody>";
-$tabla .= "</table>";
+    $tabla .= "</tbody>";
+    $tabla .= "</table>";
 
-echo $tabla;
-?>
+    echo $tabla;
+    ?>
+</h4>
 </body>
 </html>
