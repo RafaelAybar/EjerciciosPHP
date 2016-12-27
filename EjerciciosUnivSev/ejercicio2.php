0124b42bd5157aba5a6b4ddaa7010f8ec3c1be40
Rafael Aybar <rafaelaybar@hotmail.com>
2016-12-27 12:03:01 +0100

    Ahora el script es funcional

--- EjerciciosUnivSev/ejercicio2.php
+++ EjerciciosUnivSev/ejercicio2.php
@@ -1,6 +1,21 @@
 <?php
     //Declaramos la variable y forzamos a tipo cadena
-    $nombrear = $_POST['ext'];
-    //Comprobación y lógica del ejercicio
-    echo " Tu archivo es $nombrear";
+    $nombrear =  $_POST['ext'];
+    //Separamos la extensión del archivo que sigue a partir del punto
+    echo " Tu archivo es $nombrear </br>";
+    $trozo = explode(".", $nombrear);
+    $exten = end($trozo);
+    //Comprobamos cada valor posible de la extensión
+    if ($exten == "pdf") {
+        echo "Esta extensión pertenece a Acrobat Reader";
+    }
+    elseif ($exten == "odt") {
+        echo "Esta extensión pertenece a un texto de Open Document";
+    }
+    elseif ($exten == "pptx") {
+        echo "Esta extensión es de una presentación de Microsof Office";
+    }
+    else {
+        echo "Esa extensión no está registrada";
+    }
 ?>
