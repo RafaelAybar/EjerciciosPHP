f5f3a01fcbc326958a7c529dd4d1f621053e72d5
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-02-03 09:37:19 +0100

    Create 03_ejercicio.php

---
+++ Practicarecup/03_ejercicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title> Programación en PHP</title>
+    </head>
+<body>
+    <?php
+        function test() {
+            $mivar = "Hola mundo";
+            echo "$mivar";
+        }
+        echo test();
+    ?>
+</body>
+</html>
