3d0ef8709421c060b178b6cfb756b2a10db9fbf8
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-04 12:07:45 +0100

    Terminados los ejercicios

--- Practicarecup/02_ejercicio.php
+++ Practicarecup/02_ejercicio.php
@@ -7,31 +7,43 @@
 <body>
     <?php
      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-         echo "Estamos usando POST";
+         echo "Estamos usando POST </br>";
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-         echo "Estamos usando GET";
-         //ESTA es la opción por defecto
+         echo "Estamos usando GET </br>";
+         //ESTA es la opción por defecto, aunque no hayamos seleccionado nada
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-         echo "Estamos usando PUT";
+         echo "Estamos usando PUT </br>";
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-         echo "Estamos usando DELETE";
+         echo "Estamos usando DELETE </br>";
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'TRACE') {
-         echo "Estamos usando TRACE";
+         echo "Estamos usando TRACE </br>";
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
-         echo "Estamos usando HEAD";
+         echo "Estamos usando HEAD </br>";
      }
      elseif ($_SERVER['REQUEST_METHOD'] == 'CONNECT') {
-         echo "Estamos usando CONNECT";
+         echo "Estamos usando CONNECT</br>";
+     }
+     elseif ($_SERVER['REQUEST_METHOD'] == 'CONNECT') {
+         echo "Estamos usando CONNECT</br>";
+     }
+     elseif ( $_SERVER['HTTP_COOKIE'] == 'HTTP_COOKIE') {
+         echo "Comprobamos las cookies";
      }
      else {
-         echo "No estamos usando nada";
+         echo "No estamos usando nada </br>";
      }
-     echo $_SERVER['DOCUMENT_ROOT']; 
-    ?>
+     echo $_SERVER['DOCUMENT_ROOT'];
+     echo "</br>";
+     echo $_SERVER['HTTP_USER_AGENT'];
+     echo "</br>";
+     //con getcwd obtenemos el directorio actual
+     echo getcwd();
+     echo "</br>";
+?>
 </body>
 </html>
--- Practicarecup/04_ejercicio.php
+++ Practicarecup/04_ejercicio.php
@@ -11,11 +11,13 @@
    <input type="submit" value="Enviar">
    <input type="reset" value="Reestablecer"></br>
     <?php
+    //Controlamos los errores que se crean al tener todo en el mismo archivo
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
         //apartado a
         const SALUDO = "Hola, bienvenido a nuestra web";
         echo SALUDO;
         echo "</br>";
-        //apartado b
+        //apartado b (no se especifica de que figura hay que hacerlo)
         $base = $_POST['base'];
         $altura = $_POST['altura'];
         if (isset($base) && isset($altura)) {
@@ -26,8 +28,23 @@
         echo area($base,$altura)."</br>";
         }
         else {
-            echo "No has definido nada";
+            echo "No has definido nada </br>";
         }
+        //apartado c
+        const LUZ = 299792458;
+        echo "La velocidad de la luz es ".LUZ. "metros por segundo </br>";
+        //apartado d
+        const PHI = 1.618;
+        if ($base > $altura) {
+              function aureo ($base) {
+                    $producto = PHI*$base;
+                    echo "La proporción es ".$producto;
+                            }
+            echo aureo($base);
+        }
+        else {
+                echo "No se ha cumplido la condicion de que la base es mayor que la altura";
+            }
     ?>
 </body>
 </head>
