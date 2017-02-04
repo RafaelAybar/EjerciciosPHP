f9716659145cd10ed21bf756e2b6138b81c58e13
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-04 13:46:18 +0100

    Ejercicios

---
+++ Practicarecup/06_ejercicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title> Mi primer script en PHP</title>
+    </head>
+<body>
+    <?php
+        echo "1 </br>";
+        echo "2 </br>";
+        echo "3 </br>";
+        echo "4 </br>";
+        echo "5 </br>";
+        echo "6 </br>";
+        echo "7 </br>";
+        echo "8 </br>";
+        echo "9 </br>";
+        echo "10 </br>";
+    ?>
+</body>
+</html>
---
+++ Practicarecup/07_ejercicio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title> Mi primer script en PHP</title>
+    </head>
+<body>
+    <?php
+    //Apartado 1
+    $potencia = -155*10^300;
+    echo "El número es $potencia </br>";
+    //Apartado 2
+    $juego = "¡Ha ganado 100€ en nuestro juego";
+    echo "$juego </br>";
+    //Apartado 3
+    $a = true;
+    $b = false;
+    $c = true;
+    if ($a == true && $b == false && $c == true) {
+        echo "La variable a es verdadera, la b es falsa y c es verdadera </br>";
+        echo "La condición c no puede cumplirse,l al ser a verdadero y b falso </br>";
+    }
+    //Apartado 4
+    $num1oct = 034273557;
+    $num2oct = 013234;
+    $x = $num1oct+$num2oct;
+    echo "La suma es $x </br>";
+    ?>
+</body>
+</html>
