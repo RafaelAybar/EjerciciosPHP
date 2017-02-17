9a004a99e4c56ef9dcb9057ac434319d15047350
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-02-17 10:20:37 +0100

    Completado formulario pero no coge las variables

--- Practicarecup/Práctica2/02_ejercicio.php
+++ Practicarecup/Práctica2/02_ejercicio.php
@@ -9,8 +9,8 @@
         <table>
             <tr>
                 <td> Mostrar imagen<input type="image" name="imagen"></td>
-                <td>Subir imagen <input type="file" name="subir"></td>
-                <td>Hora <input type="datetime" name="hora"> </td>
+                <td>Subir rchivos <input type="file" name="subir"></td>
+                <td>Hora <input type="time" name="hora"> </td>
             </tr>
             <tr>
                 <td>Nombre Completo <input type="text" name="ape"></td>
@@ -28,7 +28,7 @@
                 <td>Fecha nacimiento <input type="date" name="fenac"></td>
                 <td>Color Favorito <input type="color" name="color"></td>
                 <td>Color Ojos 
-                        <select>
+                        <select multiple name="coloroj[]">
                         <option value="marrones">Marrones</option>
                         <option value="verdes">Verdes</option>
                         <option value="negros">Mercedes</option>
@@ -38,12 +38,62 @@
             </tr>
             <tr>
                 <td>Hobbies: </br>
-                    Ocio <input type="checkbox" name="musica"></td></br>
-                    Deporte<input type="checkbox" name="deporte"></br>
-                    
-
+                    Música <input type="checkbox" name="hobby[]"></br>
+                    Deporte<input type="checkbox" name="hobby[]"></br>
+                    TV <input type="checkbox" name="hobby[]"> </br>
+                    Cine <input type="checkbox" name="hobby[]"></br>
+                    Naturaleza <input type="checkbox" name="hobby[]"></br>
+                </td>
+                <td>Sexo: 
+                Hombre<input type="radio" name="sex[]" value="Hombre">
+                Mujer<input type="radio" name="sex[]" value="Mujer">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Comentarios:<textarea rows="25" cols="25" name="texto"></textarea></td>
+            </tr>
+            <tr><td><input type="submit" value="Enviar"><input type="reset" value="Reiniciar datos"></td>
             </tr>
         </table>
         <?php
+            
+            $imagen = $_GET['imagen'];
+            $subir = $_GET['subir'];
+            $hora = $_GET['hora'];
+            $ape = $_GET['ape'];
+            $usuario = $_GET['usu'];
+            $pass = $_GET['pass'];
+            $oculto = $pass;
+            $email = $_GET['email'];
+            $telefono = $_GET['telef'];
+            $peso = $_GET['peso'];
+            $altura = $_GET['alt'];
+            $fenac = $_GET['fenac'];
+            $colorfav = $_GET['color'];
+            $coloroj = $_GET['coloroj'];
+            $hobbies = $_GET['hobby'];
+            $sex = $_GET['sex'];
+            $coment = $_GET['texto'];
+            if (isset ($imagen) && isset($subir) && isset($hora) && isset($altura) && isset($ape)&& isset($fenac) && isset($pass)&& isset($peso) && isset($colorfav)&& isset($coloroj)&& isset($telefono)&& isset($coment)&&isset($sex)) {
+                echo " $imagen</br>";
+                echo "$subir</br>";
+                echo "Fecha nacimiento $fenac</br>";
+                echo "$ape</br>";
+                echo "El nombre de usuario es$usuario</br>";
+                echo "La contraseña es $pass</br>";
+                echo "El campo oculto es  $oculto</br>";
+                echo "El nº de teléfono es $telefono</br>";
+                echo "El peso es $peso</br>";
+                echo "La altura es $altura </br>";
+                echo "La hora introducda es $hora</br>";
+                echo "El color favorito es $colorfav</br>";
+                echo "El color de ojos es $coloroj</br>";
+                echo "Tu sexo es $sex</br>";
+                echo "Tu opinión es $coment</br>";
+            }
+            else {
+                echo "Debes rellenar todos los campos";
+            }
         ?>
     </body>
