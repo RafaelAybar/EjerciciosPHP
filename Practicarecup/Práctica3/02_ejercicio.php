0ca9fb4bd73e82f8fbceda817f6ebe9ccf3c39ef
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-23 23:53:08 +0100

    Arreglado

--- Practicarecup/Práctica3/02_ejercicio.php
+++ Practicarecup/Práctica3/02_ejercicio.php
@@ -7,7 +7,21 @@ $apellidos2 = array("Quesada", "Alcala", "Marín", "Suarez", "Cobos","Rios");
 $randn = array_rand($nombres);
 $radn2 = array_rand($apellidos1);
 $rand3 = array_rand($apellidos2);
-echo "Nombre aleatorio ". $nombres[$randn] 
-    ." apellido aleatorio ". $apellidos1[$radn2] 
-    ." segundo apellido aleatorio ". $apellidos2[$rand3];
+echo "<table>";
+    echo "<tr>";
+    echo "<td>";
+echo "Nombre aleatorio ". $nombres[$randn] ;
+    echo "</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>";
+echo " Apellido aleatorio ". $apellidos1[$radn2]; 
+    echo "</tr>";
+    echo "</td>";
+    echo "<tr>";
+    echo "<td>";
+echo " Segundo apellido aleatorio ". $apellidos2[$rand3];
+    echo "</tr>";
+    echo "</td>";
+echo "</table>";
 ?>
