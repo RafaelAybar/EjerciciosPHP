7c12b363b491baf56537b07374f7102a78d6f28a
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-23 23:44:32 +0100

    Completado

--- Practicarecup/Práctica3/02_ejercicio.php
+++ Practicarecup/Práctica3/02_ejercicio.php
@@ -1,16 +1,13 @@
 <?php
-echo "Los arrays originales son </br>";
+
 $nombres = array("Pepe", "Carlos", "Jesús", "Lola", "Rosa", "Maria");
 $apellidos1 = array("Martin", "Lopez", "Salas", "Mateo", "Abas", "De Diego");
 $apellidos2 = array("Quesada", "Alcala", "Marín", "Suarez", "Cobos","Rios");
-    print_r($nombres);
-    echo "</br>";
-    print_r($apellidos1);
-    echo "</br>";
-    print_r($apellidos2);
-    echo "</br>";
 
-
-$nomalet = [mt_rand(0,5)] = array_merge($nombres,$apellidos1,$apellidos2);
-print_r($nomalet);
+$randn = array_rand($nombres);
+$radn2 = array_rand($apellidos1);
+$rand3 = array_rand($apellidos2);
+echo "Nombre aleatorio ". $nombres[$randn] 
+    ." apellido aleatorio ". $apellidos1[$radn2] 
+    ." segundo apellido aleatorio ". $apellidos2[$rand3];
 ?>
