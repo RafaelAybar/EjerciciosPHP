6a2d3138c8169984ff929380d70d136918c95895
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-24 10:56:30 +0100

    Arreglado

--- Practicarecup/Práctica3/03_ejercicio.php
+++ Practicarecup/Práctica3/03_ejercicio.php
@@ -1,27 +1,7 @@
 <?php
-$mes = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-echo $mes[0]. " 31 días";
-echo "</br>";
-echo $mes[1]. " 28 días";
-echo "</br>";
-echo $mes[2]. " 31 días";
-echo "</br>";
-echo $mes[3]. " 30 días";
-echo "</br>";
-echo $mes[4]. " 31 días";
-echo "</br>";
-echo $mes[5]. " 30 días";
-echo "</br>";
-echo $mes[6]. " 31 días";
-echo "</br>";
-echo $mes[7]. " 31 días";
-echo "</br>";
-echo $mes[8]. " 31 días";
-echo "</br>";
-echo $mes[9]. " 30 días";
-echo "</br>";
-echo $mes[10]. " 31 días";
-echo "</br>";
-echo $mes[11]. " 31 días";
+echo "El array resulatante es:</br>";
+$mes = array('enero'=>"31 dias",'febrero'=>"28",'marzo'=>"31",'abril'=>"30",'mayo'=>"31",'junio'=>"30",'julio'=>"31", 'agosto'=>"31",'septiembre'=>"30",'octubre'=>"31",'noviembre'=>"30",'diciembre'=>"31");
+print_r($mes);
+
 
 ?>
