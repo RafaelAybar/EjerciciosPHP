1ef3a132f530e61dc3aa8b6d7704695aa558dae8
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-24 10:40:26 +0100

    Terminado

--- Practicarecup/Práctica3/03_ejercicio.php
+++ Practicarecup/Práctica3/03_ejercicio.php
@@ -1,2 +1,27 @@
 <?php
+$mes = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+echo $mes[0]. " 31 días";
+echo "</br>";
+echo $mes[1]. " 28 días";
+echo "</br>";
+echo $mes[2]. " 31 días";
+echo "</br>";
+echo $mes[3]. " 30 días";
+echo "</br>";
+echo $mes[4]. " 31 días";
+echo "</br>";
+echo $mes[5]. " 30 días";
+echo "</br>";
+echo $mes[6]. " 31 días";
+echo "</br>";
+echo $mes[7]. " 31 días";
+echo "</br>";
+echo $mes[8]. " 31 días";
+echo "</br>";
+echo $mes[9]. " 30 días";
+echo "</br>";
+echo $mes[10]. " 31 días";
+echo "</br>";
+echo $mes[11]. " 31 días";
+
 ?>
