29cc3f5b0128d27c78e98c96c799797defa532aa
Rafael Aybar <rafaelaybar@hotmail.com>
2017-03-21 17:43:27 +0100

    2

---
+++ Ejerciciosclase/EjerciciosRecup/ej2.php
@@ -0,0 +1,10 @@
+<?php
+    //un for para las filas y otro para las columnas
+    //no he conseguido ponerlo en forma de tabla
+   for ($b=1;$b<=10;$b++)
+   {
+       for ($a=1;$a<=$b;$a++)
+   {echo '#';}
+    echo '<br>';
+   } 
+?>
