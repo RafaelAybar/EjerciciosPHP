176be14948234ba5c7503ffd78e47e3fba5ac515
Maripuri Seg Dia <Maripuri Seg Dia>
2017-04-18 09:33:14 +0200

    Terminado el ejercicio 2

--- Practicarecup/PracticaJun/02_ejformulario.php
+++ Practicarecup/PracticaJun/02_ejformulario.php
@@ -12,43 +12,7 @@
     </head>
     <body>
         <center>
-        <h2>Formulario validado por campos</h2>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-             <table>
-                 <tr>
-                     <td>Nombre:</td>
-                 </tr>
-                 <tr>
-                     <td><input type="text" name="nom" required></td>
-                 </tr>
-                 <tr>
-                     <td>Correo:</td>
-                 </tr>
-                 <tr>
-                     <td><input type="email" name="correo" required></td>
-                 </tr>
-                 <tr>
-                     <td>Tecnologías:</td>
-                 </tr>
-                 <tr>
-                     <td>PHP<input type="radio" name="radio" value="PHP" checked>
-                     HTML<input type="radio" name="radio" value="HTML">
-                     CSS <input type="radio"name="radio" value="CSS"></td>
-                 </tr>
-                 <tr>
-                     <td>Mensaje:</td>
-                 </tr>
-                 <tr>
-                     <td><textarea rows="15" cols="25" name="texto"></textarea></td>
-                 </tr>
-                 <tr>
-                     <td>Resumen: <input type="file" name="resumen" required></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" value="Enviar" id="submit"></td>
-                </tr>
-             </table>
-             <?php
+        <?php
     error_reporting(E_ERROR | E_WARNING | E_PARSE);
     $nombre = trim($_POST['nom']);
     $correo = trim($_POST['correo']);
@@ -60,13 +24,65 @@ if (isset($nombre) && isset($correo) && isset($texto) && isset($archivo) && isse
     stripcslashes($nombre);
     stripcslashes($correo);
     stripcslashes($texto);
+    if (empty($nombre)) {
+        $errornom="Debes introducir el nombre";
+        }
+    elseif (empty($correo)) {
+            $errormail="Debes introducir el correo";
+        }
+    elseif (empty($texto)) {
+            $errortext="Debes introducir el el texto";
+        }
+    elseif (empty($archivo)) {
+            $errorarchivo="Debes introducir el archivo";
+        }
+    elseif (empty($radio)) {
+            $errprradio="Debes seleccionar una opción";
+        }
     echo "El nombre es $nombre, tu email es $correo,has seleccionado $radio, has subido el archivo $archivo y el texto introducido es:"."</br>";
-echo "$texto";
+    echo "$texto";
 }
 else {
-    die("No has introducido los campos obligatorios");
+    die("Debes introducir todos los campos");
 }
 ?>
+        <h2>Formulario validado por campos</h2>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+             <table>
+                 <tr>
+                     <td>Nombre:</td>
+                 </tr>
+                 <tr>
+                     <td><input type="text" name="nom" required><font color=red> <?php echo "$errornom";?></font></td>
+                 </tr>
+                 <tr>
+                     <td>Correo:</td>
+                 </tr>
+                 <tr>
+                     <td><input type="email" name="correo" required> <font color=red> <?php echo "$errormail";?></font></td>
+                 </tr>
+                 <tr>
+                     <td>Tecnologías:</td>
+                 </tr>
+                 <tr>
+                     <td>PHP<input type="radio" name="radio" value="PHP" checked>
+                     HTML<input type="radio" name="radio" value="HTML">
+                     CSS <input type="radio"name="radio" value="CSS"> <font color=red> <?php echo "$errprradio";?></font></td>
+                 </tr>
+                 <tr>
+                     <td>Mensaje:</td>
+                 </tr>
+                 <tr>
+                     <td><textarea rows="15" cols="25" name="texto"></textarea><font color=red> <?php echo "$errortext";?></font></td>
+                 </tr>
+                 <tr>
+                     <td>Resumen: <input type="file" name="resumen" required><font color=red> <?php echo "$errorarchivo";?></font></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Enviar" id="submit"></td>
+                </tr>
+             </table>
+             
     </center>
     </body>
 </html>
