9dbb012940989430cfad062ce6bfcb7d2dbceb1c
Rafael Aybar <rafaelaybar@hotmail.com>
2017-10-04 09:13:22 +0200

    Nuevos ejercicios

---
+++ ejercicios2/constantes.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hola mundo <br>";
+    print "<hr>";
+    echo "No me 'escucháis' <br>";
+    /*Definimos una constante (posición de memoria que )almacena un valor inalterable
+    mientras se ejecute el script PHP)
+    */
+    define("EuroPta", 166.389);
+    define("PtaEuro", 1/166.386);
+    echo "Un euro son: ", EuroPta, "<br><hr>";
+    echo "Una peseta es: ", PtaEuro, " euros <br>";
+    //Definimos una var
+?>
