b8183f084746e407f71a65029369fe05326324ed
Rafael Aybar <rafaelaybar@hotmail.com>
2017-10-04 09:49:41 +0200

    ...

--- ejercicios2/variables.php
+++ ejercicios2/variables.php
@@ -18,4 +18,42 @@
     echo __FILE__."<br>";
     echo __LINE__."<br>";
     echo PHP_OS;
+    // Variables estáticas ()
+    function prueba ()
+    {
+        $c = 0;
+        $b = 0;
+        echo "<br>";
+        echo $b."<br>";;
+        $c+=5;
+        $b-=7;
+        echo "$c y $b <br>";
+    }
+    function prueba1 ()
+    {
+        $c = 0;
+        static $b = 0;
+        echo "<br>";
+        echo $c."<br>";;
+        $c+=5;
+        $b-=7;
+        echo "$c y $b <br>";
+    }
+    echo prueba();
+    echo "<br>";
+    echo prueba1();
+    echo "Segunda ejecución <br>";
+    echo prueba();
+    echo "<br>";
+    echo prueba1();
+
+    //Declarar variable de variable
+    $color = "rojo";
+    $$color = "es mi color favorito";
+    //el valor es $rojo por que se coge el nombre del color
+    //$color ="amarillo"; Se ejecuta la última
+    print $$color;
+    print ${$color};
+    print $rojo;
+
 ?>
