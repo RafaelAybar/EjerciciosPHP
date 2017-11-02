a050b6c25a9b83a9412ab423a1c5bb3021893590
Rafael Aybar <rafaelaybar@hotmail.com>
2017-11-02 10:51:42 +0100

    actualizado

--- ejercicios2/ejerciciosficheros.php
+++ ejercicios2/ejerciciosficheros.php
@@ -1,22 +1,16 @@
 <?php
     //abrimos el fichero en modo lectura/escritura y comprobamos que tiene una ruta válida
     //ojo a la función is_readable();
-    //$nomfichero = "/var/WWWW/html/EjerciciosPHP/ejercicios2/ejemplo.txt";
-    //if (is_writable($nomfichero)) {
-        /*$fichero=fopen("/var/WWWW/html/EjerciciosPHP/ejercicios2/ejemplo.txt", "r+", USE_INCLUDE_PATH);
+    $nomfichero = "/var/www/html/EjerciciosPHP/ejercicios2/ejemplo.txt";
+    if (is_writable($nomfichero)) {
+       $fichero=fopen("/var/www/html/EjerciciosPHP/ejercicios2/ejemplo.txt", "r+", USE_INCLUDE_PATH);
         if ($fichero === false || $fichero ===1 || $fichero === null) {
             die("Hubo un error");
         }
         fwrite($fichero,'Esta frase sobreescribe la anterior');
-        fclose($fichero);*/
-    //}
-    //else {
-      //  echo "No se puede escribir";
-    //}
-    $f = fopen("/var/WWWW/html/EjerciciosPHP/ejercicios2/ejemplo.txt", "w+");
-    fwrite($f, "hola");
-    while (!feof($f)) {
-        echo fgetc($f);
+        fclose($fichero);
     }
-    fclose($f);
+    else {
+      echo "No se puede escribir";
+    }
 ?>
---
+++ ejercicios2/palabras.php
@@ -0,0 +1,26 @@
+<?php
+    //Volcar el fichero en un array asociativo y mostrar su contenido en pantalla
+    if (is_file("/var/www/html/EjerciciosPHP/ejercicios2/palabras.txt")) {
+        $f = fopen('palabras.txt', 'r+');
+        //Guardamos el contenido del array
+        
+        while (!eof($f)) {
+            $linea = fgets($f);
+            echo "$linea";
+        }
+        fclose($f);
+   }
+   else {
+        die("No es un fichero");
+    }
+
+    //Mostrar en pantalla el fichero ordenado alfabéticamente
+    $fichero = fopen('palabras.txt','r+');
+    $fich = file($fichero);
+    sort($fich);
+    foreach ($fich as $key => $value) {
+        echo $value;
+    }
+    // crear una función llamada contar(fichero,caracter) que muestre en pantalla el numero de veces que caracter aparece en el fichero
+
+?>
